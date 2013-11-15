e9a295d43835033790f299586c98751a36a46756
Shapoval <johnnysk911@gmail.com>
2013-11-15 11:11:38 +0200

    fixed code by PSR standart

--- application/core/View.php
+++ application/core/View.php
@@ -1,9 +1,10 @@
 <?php
 
-class View{
-
-    public function display($content,$data=array()){
-        $module=Registry::getValue('module');
+class View
+{
+    public function display($content, $data = array())
+    {
+        $module = Registry::getValue('module');
         include DIR."/template.php";
     }
 }

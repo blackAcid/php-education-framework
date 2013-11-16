13f3e115d77f7726dbf8b4e3eea12787875702b4
anna <black_acid@mail.ru>
2013-11-16 12:50:21 +0200

    edited by PSR2 standard

--- application/core/View.php
+++ application/core/View.php
@@ -1,17 +1,9 @@
 <?php
-<<<<<<< HEAD
-=======
-
->>>>>>> 991657429e4c727735fb5e98f5fc966db0158a50
 class View
 {
     public function display($content, $data = array())
     {
-<<<<<<< HEAD
-        $module=Registry::getValue('module');
-=======
         $module = Registry::getValue('module');
->>>>>>> 991657429e4c727735fb5e98f5fc966db0158a50
         include DIR."/template.php";
     }
 }
